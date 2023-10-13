95a9da4b2ec161bf7ac9c6d0564c88fc04ae6fad
fhssouza <fhssouza@gmail.com>
2023-10-13 18:44:44 -0300

    Post de Users, recebendo e devolvendo Json

---
+++ src/main/java/com/souzatech/quarkussocial/rest/UserResource.java
@@ -0,0 +1,22 @@
+package com.souzatech.quarkussocial.rest;
+
+import com.souzatech.quarkussocial.rest.dto.CreateUserRequest;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class UserResource {
+    @POST
+    public Response createUser(CreateUserRequest userRequest){
+        return Response.ok().build();
+    }
+    @GET
+    public Response listAllUsers(){
+        return Response.ok().build();
+    }
+
+}
