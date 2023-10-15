13ecb0132fe0c63fc86ebfe68df2adcfc13ddc3e
fhssouza <fhssouza@gmail.com>
2023-10-15 17:36:35 -0300

    Criando o Resource de Posts e os endpoints

---
+++ src/main/java/com/souzatech/quarkussocial/rest/PostResource.java
@@ -0,0 +1,22 @@
+package com.souzatech.quarkussocial.rest;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/users/{userId}/posts")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class PostResource {
+
+    @POST
+    public Response savePost(){
+        return Response.status(Response.Status.CREATED).build();
+    }
+
+    @GET
+    public Response listPosts(){
+        return Response.ok().build();
+    }
+
+}
