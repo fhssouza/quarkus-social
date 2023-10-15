c30be3e743c5a5b7678634d8cc4ce1fabb7098b4
fhssouza <fhssouza@gmail.com>
2023-10-15 17:02:49 -0300

    Evitando codigo boilerplate com o Lombok project

--- src/main/java/com/souzatech/quarkussocial/rest/dto/CreateUserRequest.java
+++ src/main/java/com/souzatech/quarkussocial/rest/dto/CreateUserRequest.java
@@ -2,26 +2,15 @@ package com.souzatech.quarkussocial.rest.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
+@Data
 public class CreateUserRequest {
+
     @NotBlank(message = "Name is Required")
     private String name;
+
     @NotNull(message = "Age is Required")
     private Integer age;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
